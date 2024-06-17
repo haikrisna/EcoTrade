54380eda90ed856df4b0311ce2efe4f9b88c9d7f
melindawijaya <127614248+melindawijaya@users.noreply.github.com>
2024-06-18 06:00:50 +0700

    Update css2 login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <link rel="stylesheet" href="globals.css" />
+        <link rel="stylesheet" href="css/globals.css" />
         <link rel="stylesheet" href="css/login.css" />
     </head>
     <body>
