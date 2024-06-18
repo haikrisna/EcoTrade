49e8aa64932a5e4895bc23d960dd68eae7b9d6cc
melindawijaya <127614248+melindawijaya@users.noreply.github.com>
2024-06-18 09:38:23 +0700

    Add files via upload

---
+++ resources/views/pesanandalamproses.blade.php
@@ -0,0 +1,350 @@
+<!DOCTYPE html>
+<html>
+<head>
+  	<meta charset="utf-8">
+  	<meta name="viewport" content="initial-scale=1, width=device-width">
+  	
+  	<link rel="stylesheet"  href="./index.css" />
+  	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Plus Jakarta Sans:wght@500;600;700&display=swap" />
+  	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Outfit:wght@500&display=swap" />
+  	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Manrope:wght@500;700;800&display=swap" />
+  	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" />
+  	
+  	
+  	
+</head>
+<body>
+  	
+  	<div class="pesanan-dalam-proses-vendor">
+    		<div class="nav-bar">
+      			<img class="active-icon" alt="" src="Active.svg">
+      			
+      			<div class="side-menu">
+        				<div class="main-menu">
+          					<div class="m-a-i">M A I N  M E N U</div>
+          					<div class="main-menu1">
+            						<div class="dashboard" id="dashboardContainer">
+              							<img class="home-icon" alt="" src="home.svg">
+              							
+              							<div class="dashboard1">Dashboard</div>
+            						</div>
+            						<div class="house">
+              							<img class="home-icon" alt="" src="vuesax/linear/car.svg">
+              							
+              							<div class="pesanan">Pesanan</div>
+            						</div>
+            						<div class="dashboard" id="insightContainer">
+              							<img class="home-icon" alt="" src="vuesax/linear/chart.svg">
+              							
+              							<div class="statistik">Statistik</div>
+            						</div>
+            						<div class="dashboard" id="orderContainer">
+              							<img class="home-icon" alt="" src="empty-wallet-change.svg">
+              							
+              							<div class="dompet">Dompet</div>
+            						</div>
+            						<div class="dashboard" id="frameContainer">
+              							<img class="home-icon" alt="" src="vuesax/linear/calendar.svg">
+              							
+              							<div class="riwayat-transaksi">Riwayat Transaksi</div>
+            						</div>
+          					</div>
+        				</div>
+        				<div class="preferences">
+          					<div class="p-r-e">P R E F E R E N C E S</div>
+          					<div class="preferences1" id="preferencesContainer">
+            						<div class="settings">
+              							<img class="home-icon" alt="" src="setting.svg">
+              							
+              							<div class="settings1">Settings</div>
+            						</div>
+          					</div>
+        				</div>
+      			</div>
+      			<div class="logout" id="logoutContainer">
+        				<img class="home-icon" alt="" src="logout.svg">
+        				
+        				<div class="log-out">Log Out</div>
+      			</div>
+    		</div>
+    		<div class="pesanan-dalam-proses-vendor-child">
+    		</div>
+    		<div class="pesanan-masuk" id="pesananMasukText">Pesanan Masuk</div>
+    		<div class="pesanan-dalam-proses-vendor-item">
+    		</div>
+    		<div class="rectangle-parent">
+      			<div class="group-child">
+      			</div>
+      			<div class="div">+2</div>
+    		</div>
+    		<b class="dalam-proses">Dalam Proses</b>
+    		<div class="rectangle-group">
+      			<div class="group-item">
+      			</div>
+      			<div class="div1">1</div>
+    		</div>
+    		<div class="ditolak" id="ditolakText">Ditolak</div>
+    		<div class="riwayat-pesanan" id="riwayatPesananText">Riwayat Pesanan</div>
+    		<div class="rectangle-container">
+      			<div class="group-inner">
+      			</div>
+      			<div class="div">1</div>
+    		</div>
+    		<div class="map-maker-sekaran-50229-gun-parent">
+      			<img class="map-maker-sekaran-50229-gun" alt="" src="🌎 Map Maker: Sekaran, 50229, Gunung Pati, Semarang, Central Java, Indonesia (Standard).png">
+      			
+      			<div class="type">
+        				<div class="jenis-limbah">Jenis Limbah</div>
+      			</div>
+      			<div class="group-wrapper">
+        				<div class="callysta-putri-parent">
+          					<b class="callysta-putri">Callysta Putri</b>
+          					<div class="group-container">
+            						<div class="star-parent">
+              							<img class="star-icon" alt="" src="Star 1.svg">
+              							
+              							<div class="div3">4.7</div>
+              							<div class="div4">( 10      )</div>
+              							<img class="group-icon" alt="" src="Group 427320622.png">
+              							
+            						</div>
+          					</div>
+        				</div>
+      			</div>
+      			<div class="group-parent">
+        				<div class="type-parent">
+          					<div class="type1">
+            						<div class="alamat">Alamat</div>
+          					</div>
+          					<div class="jl-gajah-raya-no-7-wrapper">
+            						<b class="jl-gajah-raya">Jl. Gajah Raya no 7</b>
+          					</div>
+        				</div>
+        				<div class="type2">
+          					<div class="kec-semarang-timur">Kec. Semarang Timur, Kel. Bakti, Kota Semarang</div>
+        				</div>
+      			</div>
+      			<div class="group-frame">
+        				<div class="type-group">
+          					<div class="type3">
+            						<div class="waktu-pick-up">Waktu Pick Up</div>
+          					</div>
+          					<div class="wib-wrapper">
+            						<b class="wib">10.00 WIB</b>
+          					</div>
+        				</div>
+      			</div>
+      			<div class="group-div">
+        				<div class="type-container">
+          					<div class="type3">
+            						<div class="keterangan">Keterangan</div>
+          					</div>
+          					<div class="kotoran-kambing-wrapper">
+            						<b class="kotoran-kambing">Kotoran Kambing</b>
+          					</div>
+        				</div>
+      			</div>
+      			<div class="group-wrapper1">
+        				<div class="type-container">
+          					<div class="type3">
+            						<div class="keterangan">Biaya</div>
+          					</div>
+          					<div class="rp100000-wrapper">
+            						<b class="rp100000">Rp100.000</b>
+          					</div>
+        				</div>
+      			</div>
+      			<div class="group-wrapper2">
+        				<div class="type-wrapper">
+          					<div class="type3">
+            						<div class="metode-pembayaran">Metode Pembayaran</div>
+          					</div>
+        				</div>
+      			</div>
+      			<div class="rectangle-parent1" id="groupContainer6">
+        				<div class="rectangle-div">
+        				</div>
+        				<b class="selesaikan">Selesaikan</b>
+      			</div>
+      			<div class="rectangle-parent2" id="groupContainer7">
+        				<div class="group-child1">
+        				</div>
+        				<div class="input-berat">Input Berat</div>
+      			</div>
+      			<div class="rectangle-parent3" id="groupContainer8">
+        				<div class="group-child2">
+        				</div>
+        				<div class="pantau-proses">Pantau Proses</div>
+      			</div>
+      			<div class="group-child3">
+      			</div>
+      			<b class="kotoran-ternak">Kotoran Ternak</b>
+      			<img class="user-03c-icon" alt="" src="User 03C.svg">
+      			
+      			<div class="toyota-fortuner-parent">
+        				<b class="toyota-fortuner">Open on </b>
+        				<img class="group-child4" alt="" src="Group 427320636.png">
+        				
+      			</div>
+      			<div class="shopeepay-parent">
+        				<b class="shopeepay">Shopeepay</b>
+        				<img class="image-2-icon" alt="" src="image 2.png">
+        				
+      			</div>
+    		</div>
+    		<div class="nav-bar-top">
+      			<div class="search">
+        				<div class="button">
+        				</div>
+        				<div class="search-something-here">Search something here</div>
+        				<img class="search-normal-icon" alt="" src="search-normal.svg">
+        				
+        				<img class="vuesaxlinearfilter-icon" alt="" src="vuesax/linear/filter.svg">
+        				
+      			</div>
+      			<img class="image-106-icon" alt="" src="image 106.png">
+      			
+      			<div class="profil-notification">
+        				<img class="notification-icon" alt="" src="Notification.svg">
+        				
+      			</div>
+    		</div>
+  	</div>
+  	
+  	<div id="logoutConfirmContainer" class="popup-overlay" style="display:none">
+    		
+    		<div class="logout-confirm">
+      			<div class="anda-yakin-ingin">Anda yakin ingin log out?</div>
+      			<div class="rectangle-parent4" id="popupgroupContainer">
+        				<div class="group-child5">
+        				</div>
+        				<div class="yes">Yes</div>
+      			</div>
+      			<div class="rectangle-parent5" id="popupgroupContainer1">
+        				<div class="group-child6">
+        				</div>
+        				<div class="no">No</div>
+      			</div>
+    		</div>
+    		
+    		
+  	</div>
+  	
+  	
+  	
+  	<script>
+    		var popupgroupContainer = document.getElementById("popupgroupContainer");
+    		if(popupgroupContainer) {
+      			popupgroupContainer.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}
+    		
+    		var popupgroupContainer1 = document.getElementById("popupgroupContainer1");
+    		if(popupgroupContainer1) {
+      			popupgroupContainer1.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}
+    		
+    		var dashboardContainer = document.getElementById("dashboardContainer");
+    		if(dashboardContainer) {
+      			dashboardContainer.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}
+    		
+    		var insightContainer = document.getElementById("insightContainer");
+    		if(insightContainer) {
+      			insightContainer.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}
+    		
+    		var orderContainer = document.getElementById("orderContainer");
+    		if(orderContainer) {
+      			orderContainer.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}
+    		
+    		var frameContainer = document.getElementById("frameContainer");
+    		if(frameContainer) {
+      			frameContainer.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}
+    		
+    		var preferencesContainer = document.getElementById("preferencesContainer");
+    		if(preferencesContainer) {
+      			preferencesContainer.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}
+    		
+    		var logoutContainer = document.getElementById("logoutContainer");
+    		if(logoutContainer) {
+      			logoutContainer.addEventListener("click", function () {
+        				var popup = document.getElementById("logoutConfirmContainer");
+        				if(!popup) return;
+        				var popupStyle = popup.style;
+        				if(popupStyle) {
+          					popupStyle.display = "flex";
+          					popupStyle.zIndex = 100;
+          					popupStyle.backgroundColor = "rgba(113, 113, 113, 0.3)";
+          					popupStyle.alignItems = "center";
+          					popupStyle.justifyContent = "center";
+        				}
+        				popup.setAttribute("closable", "");
+        				
+        				var onClick = popup.onClick || function(e) {
+          					if(e.target === popup && popup.hasAttribute("closable")) {
+            						popupStyle.display = "none";
+          					}
+        				};
+        				popup.addEventListener("click", onClick);
+      			});
+    		}
+    		
+    		var pesananMasukText = document.getElementById("pesananMasukText");
+    		if(pesananMasukText) {
+      			pesananMasukText.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}
+    		
+    		var ditolakText = document.getElementById("ditolakText");
+    		if(ditolakText) {
+      			ditolakText.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}
+    		
+    		var riwayatPesananText = document.getElementById("riwayatPesananText");
+    		if(riwayatPesananText) {
+      			riwayatPesananText.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}
+    		
+    		var groupContainer6 = document.getElementById("groupContainer6");
+    		if(groupContainer6) {
+      			groupContainer6.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}
+    		
+    		var groupContainer7 = document.getElementById("groupContainer7");
+    		if(groupContainer7) {
+      			groupContainer7.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}
+    		
+    		var groupContainer8 = document.getElementById("groupContainer8");
+    		if(groupContainer8) {
+      			groupContainer8.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}</script></body>
+</html>
